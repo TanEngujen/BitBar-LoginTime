41303f26486baf3b71d9bca2ad523c42bf6ca5db
Tan Nhat Nguyen <me@tan-nguyen.de>
2016-02-09 15:10:02 +0100

    LoginTime script added
    - Added the BitBar-Plugin

---
+++ logintime.1h.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Get the first and latest login time of the day.
+# This plugin refresh every 1 hour.
+#
+# by Tan Nhat Nguyen me@tan-nguyen.de
+#
+# Tweak it for your benefits.
+
+# Metadata for BitBar
+# <bitbar.title>Login Time</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Tan Nhat Nguyen</bitbar.author>
+# <bitbar.author.github>TanNhatNguyen</bitbar.author.github>
+# <bitbar.desc>Shows the first and the latest login time of the day.</bitbar.desc>
+# <bitbar.abouturl>https://github.com/TanNhatNguyen/BitBar-LoginTime</bitbar.abouturl>
+
+LOGFILEPATH="/var/log/accountpolicy.log"
+
+function getLoginData
+{
+    if [ "$1" == "first" ] ; then
+        head -n1 $LOGFILEPATH | cut -d " " -f4
+    elif [ "$1" == "last" ] ; then
+        tail -n1 $LOGFILEPATH | cut -d " " -f4
+    fi
+}
+
+echo "LoginTime"
+
+echo "---"
+echo "First Login: | $FONT color=orange"
+getLoginData first
+echo "---"
+
+echo "---"
+echo "Last Login:| $FONT color=orange"
+getLoginData last
+echo "---"
